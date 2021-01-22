9c1aa1e7b6584f22e2da48f8b4a1016e063d0003
Khallekan <okeolalekanisaac@gmail.com>
2021-01-22 04:31:31 +0100

    Tours done

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -1,11 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
-function App() {
-  return <h2>Tours Project Setup</h2>
-}
+const url = 'https://course-api.com/react-tours-project';
+const App = () => {
+  const [tours, setTours] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default App
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    try {
+      const resp = await fetch(url);
+      if (resp.status >= 200 && resp.status <= 299) {
+        const data = await resp.json();
+        setIsLoading(false);
+        console.log(await data);
+        return setTours(data);
+      } else {
+        setIsLoading(false);
+        setIsError(true);
+        throw new Error(resp.statusText);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  return (
+    <>
+      {!isLoading && tours.length === 0 && (
+        <main>
+          <div className='title'>
+            <h2>no tours left</h2>
+            <button className='btn' onClick={() => fetchTours()}>
+              refresh
+            </button>
+          </div>
+        </main>
+      )}
+      {isLoading && <Loading />}
+      {isError && <h2>Error ...</h2>}
+      {!isLoading && !isError && tours.length !== 0 && (
+        <main>
+          <Tours tours={tours} removeTour={removeTour} />
+        </main>
+      )}
+    </>
+  );
+};
+
+export default App;
--- 2-tours/setup/src/Tours.js
+++ 2-tours/setup/src/Tours.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({ tours, removeTour }) => {
+  // console.log(tours);
+  // console.log(removeTour);
+
+  // const [id, image, info, name, price] = tours;
+  return (
+    <section>
+      <div className='title' style={{ marginTop: '4rem' }}>
+        <h2>our tours</h2>
+        <div className='underline'></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
